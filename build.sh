bfdd59a9cc0a2b5435ea1b8f5fb7196387bb3edb
Kévin Roussel <Kevin.Roussel@inria.fr>
2016-04-15 19:20:40 +0200

    Version "de soutenance" approuvée par les rapporteurs

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pdflatex these-KR-principal.tex
+bibtex these-KR-principal
+pdflatex these-KR-principal.tex
+pdflatex these-KR-principal.tex
---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.flg
+rm *.lof
+rm *.log
+rm *.lot
+rm *.toc
+# rm *.cb*
+
